7d335ae06fa7876eac669a32cee819651b56155f
HNDuy Nguyen <duynguyen11062001@gmail.com>
2023-06-21 14:53:01 +0300

    Add npcs data

--- fe/src/components/Home.js
+++ fe/src/components/Home.js
@@ -5,21 +5,23 @@ import bg from '../images/bg.png'
 
 const Home = () => {
   return (
-    <div className='Homepage'>
+    <div>
       <Navbar />
-      <img className='bg' src={'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/48494f72-4f45-40a7-8dac-9c9e478ef481/d57536l-e3620b9a-9bfa-40f6-8f8a-ed4cccbfacd8.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzQ4NDk0ZjcyLTRmNDUtNDBhNy04ZGFjLTljOWU0NzhlZjQ4MVwvZDU3NTM2bC1lMzYyMGI5YS05YmZhLTQwZjYtOGY4YS1lZDRjY2NiZmFjZDgucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.0pyTojW_AbWaePli2Tu7Dd43Y2lagF_IlYywlgeKiwE'} alt='background'/>
-      <h1>"IT'S DANGEROUS TO GO ALONE! TAKE THIS."</h1>
-      <section className='bg-image'>
-        <img src={bg} alt='bg' />
-      </section>
-      <section>
-        <h2>WELCOME!</h2>
-        <p>This website is made as a small wiki website, introducing one of the most well-known game series developed by Nintendo.</p>
-        <p className='title'>The Legend of Zelda</p>
-        <p>Here, you can learn briefly about the history of the Kingdom of Hyrule, where the story of good and evil takes place.
-          <br />You can learn about the main charaters of the series, as well as some other Non-playable characters (NPCs).
-        </p>
-      </section>
+      <div className='Homepage'>
+        <img className='bg' src={'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/48494f72-4f45-40a7-8dac-9c9e478ef481/d57536l-e3620b9a-9bfa-40f6-8f8a-ed4cccbfacd8.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzQ4NDk0ZjcyLTRmNDUtNDBhNy04ZGFjLTljOWU0NzhlZjQ4MVwvZDU3NTM2bC1lMzYyMGI5YS05YmZhLTQwZjYtOGY4YS1lZDRjY2NiZmFjZDgucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.0pyTojW_AbWaePli2Tu7Dd43Y2lagF_IlYywlgeKiwE'} alt='background' />
+        <h1>"IT'S DANGEROUS TO GO ALONE! TAKE THIS."</h1>
+        <section className='bg-image'>
+          <img src={bg} alt='bg' />
+        </section>
+        <section>
+          <h2>WELCOME!</h2>
+          <p>This website is made as a small wiki website, introducing one of the most well-known game series developed by Nintendo.</p>
+          <p className='title'>The Legend of Zelda</p>
+          <p>Here, you can learn briefly about the history of the Kingdom of Hyrule, where the story of good and evil takes place.
+            <br />You can learn about the main charaters of the series, as well as some other Non-playable characters (NPCs).
+          </p>
+        </section>
+      </div>
     </div>
   )
 }
--- fe/src/components/NPC.js
+++ fe/src/components/NPC.js
@@ -1,18 +1,69 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './Navbar'
 import './SCSS/NPC.scss'
 import botw from './Data_files/botw.json'
 import totk from './Data_files/totk.json'
+import oot from './Data_files/oot.json'
+import mm from './Data_files/mm.json'
+import { BiChevronsUp, BiChevronsDown } from 'react-icons/bi'
 
 const NPC = () => {
+    const [show1, setShow1] = useState(true)
+    const [show2, setShow2] = useState(false)
+    const [show3, setShow3] = useState(false)
+    const [show4, setShow4] = useState(false)
+
+
     return (
         <div className='npc'>
             <Navbar />
             <h1>NPC</h1>
 
-            <h3>Ocarina of Time</h3>
+            <h3>Ocarina of Time
+                <span> <button type='button' onClick={() => setShow1(!show1)}>{show1 === true ? <BiChevronsUp /> : <BiChevronsDown />}</button></span>
+            </h3>
+            {show1 && <section className='oot'>
+                {oot.map(post => {
+                    return (
+                        <div className='card mb-3' key={post.id}>
+                            <div className='row g-0'>
+                                <div className='col-md-4'>
+                                    <img className='card-img-top' src={post.img} alt='npc' />
+                                </div>
+                                <div className='col-md-8'>
+                                    <div className='card-body'>
+                                        <h4 className='card-title'>{post.name}</h4>
+                                        <p className='card-text'>{post.desc}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    )
+                })}
+            </section>}
 
-            <h3>Majora's Mask</h3>
+            <h3>Majora's Mask
+                <span> <button type='button' onClick={() => setShow4(!show4)}>{show4 === true ? <BiChevronsUp /> : <BiChevronsDown />}</button></span>
+            </h3>
+            {show4 && <section className='mm'>
+                {mm.map(post => {
+                    return (
+                        <div className='card mb-3' key={post.id}>
+                            <div className='row g-0'>
+                                <div className='col-md-4'>
+                                    <img className='card-img-top' src={post.img} alt='npc' />
+                                </div>
+                                <div className='col-md-8'>
+                                    <div className='card-body'>
+                                        <h4 className='card-title'>{post.name}</h4>
+                                        <p className='card-text'>{post.desc}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    )
+                })}
+            </section>}
 
             <h3>Wind Waker</h3>
 
@@ -20,31 +71,52 @@ const NPC = () => {
 
             <h3>Skyward Sword</h3>
 
-            <h3>Breath of the Wild</h3>
-            <section className='botw'>
+            <h3>Breath of the Wild
+                <span> <button type='button' onClick={() => setShow2(!show2)}>{show2 === true ? <BiChevronsUp /> : <BiChevronsDown />}</button></span>
+            </h3>
+            {show2 && <section className='botw'>
                 {botw.map(post => {
                     return (
-                        <div className='card' key={post.id}>
-                            <img src={post.img} alt='npc' />
-                            <h4 className='card-title'>{post.name}</h4>
-                            <p className='card-text'>{post.desc}</p>
+                        <div className='card mb-3' key={post.id}>
+                            <div className='row g-0'>
+                                <div className='col-md-5'>
+                                    <img className='card-img-top' src={post.img} alt='npc' />
+                                </div>
+                                <div className='col-md-7'>
+                                    <div className='card-body'>
+                                        <h4 className='card-title'>{post.name}</h4>
+                                        <p className='card-text'>{post.desc}</p>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     )
                 })}
-            </section>
+            </section>}
 
-            <h3>Tears of the Kingdom</h3>
-            <section className='totk'>
+
+            <h3>Tears of the Kingdom
+                <span> <button onClick={() => setShow3(!show3)}>{show3 === true ? <BiChevronsUp /> : <BiChevronsDown />}</button></span>
+            </h3>
+            {show3 && <section className='totk'>
                 {totk.map(post => {
                     return (
-                        <div className='card' key={post.id}>
-                            <img src={post.img} alt='npc' />
-                            <h4 className='card-title'>{post.name}</h4>
-                            <p className='card-text'>{post.desc}</p>
+                        <div className='card mb-3' key={post.id}>
+                            <div className='row g-0'>
+                                <div className='col-md-4'>
+                                    <img className='card-img-top' src={post.img} alt='npc' />
+                                </div>
+                                <div className='col-md-8'>
+                                    <div className='card-body'>
+                                        <h4 className='card-title'>{post.name}</h4>
+                                        <p className='card-text'>{post.desc}</p>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     )
                 })}
-            </section>
+            </section>}
         </div>
     )
 }
