3e666c08eb4d292e739457a0de85124c8b18017f
HNDuy Nguyen <duynguyen11062001@gmail.com>
2023-06-28 17:42:34 +0300

    Combine json files for npc, code organise

--- fe/src/components/Game.js
+++ fe/src/components/Game.js
@@ -4,75 +4,40 @@ import games from './Data_files/games.json'
 import remakes from './Data_files/remakes.json'
 import spinoffs from './Data_files/spinoffs.json'
 import { Link } from 'react-router-dom'
-// import { BiChevronsLeft, BiChevronsRight } from 'react-icons/bi'
 
 const Game = () => {
-    // const [current, setCurrent] = useState(0)
-    // const length = slides.length
 
-    // const nextSlide = () => {
-    //     setCurrent(current === length - 1 ? 0 : current + 1)
-    // }
+    const renderCarouselItems = (data) => {
+        return data.map((post, index) => {
+            const itemClasses = index === 0 ? 'carousel-item active' : 'carousel-item';
+            return (
+                <div className={itemClasses} key={post.id}>
+                    <div className='card'>
+                        <img src={post.Image} alt='...' />
+                        <div className='card-content'>
+                            <h4 className='card-title'> {post.Title}</h4>
+                            <span>{post.Time}</span>
+                            <p className='card-body'>{post.Content}</p>
+                            <Link to={post.Link} className='btn btn-outline-light' target='_blank'>
+                                Read more
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+            );
+        });
+    };
 
-    // const prevSlide = () => {
-    //     setCurrent(current === 0 ? length - 1 : current - 1)
-    // }
-    // console.log(current)
-    // if (!Array.isArray(slides) || slides.length <= 0) {
-    //     return null;
-    // }
 
     return (
         <div className='Games'>
             <h1 style={{ borderBottom: 'none' }} data-aos='zoom-in-down' data-aos-duration='1000'>Released Games</h1>
             <p data-aos='zoom-in-down' data-aos-delay='500' data-aos-duration='1000'>1986 - 2023</p>
 
-            {/* <h3 style={{fontSize: 23}}>main series</h3>
-            <BiChevronsLeft className='leftArrow' onClick={prevSlide} />
-            <BiChevronsRight className='rightArrow' onClick={nextSlide} />
-            <section className='origin'>
-                {games.map((slide, index) => {
-                    return (
-                        <div className={index === current ? 'slide active' : 'slide'} key={slide.id}>
-                            {index === current &&
-                                (<div className='card'>
-                                    <img src={slide.Image} alt='released game' />
-                                    <div className='card-content'>
-                                        <h4 className='card-title'> {slide.Title}</h4>
-                                        <p className='card-body'>{slide.Content}</p>
-                                    </div>
-                                </div>
-                                )
-                            }
-                        </div>
-                    )
-                })}
-            </section> */}
-            {/* <h3>Main Series</h3>
-            <span>Swipe to read</span> */}
-
+            <h3 style={{ paddingTop: 100 }} data-aos='zoom-in-up' data-aos-delay='1200' data-aos-duration='2000'>Main Series</h3>
             <section className='origin' data-aos='zoom-in-up' data-aos-delay='1200' data-aos-duration='2000'>
-                <h3>Main Series</h3>
                 <div id='carouselGames' className='carousel slide carousel-fade'>
-                    <div className='carousel-inner'>
-                        {games.map((post, index) => {
-                            const itemClasses = index === 0 ? 'carousel-item active' : 'carousel-item';
-                            return (
-                                <div className={itemClasses} key={post.id}>
-                                    <div className='card'>
-                                        <img src={post.Image} alt='...' />
-                                        <div className='card-content'>
-                                            <h4 className='card-title'> {post.Title}</h4>
-                                            <span>{post.Time}</span>
-                                            <p className='card-body'>{post.Content}</p>
-                                            <Link to={post.Link} className='btn btn-outline-light' target='_blank'>Read more</Link>
-                                        </div>
-                                    </div>
-                                </div>
-                            )
-                        })}
-
-                    </div>
+                    <div className='carousel-inner'>{renderCarouselItems(games)}</div>
                 </div>
                 <button className='carousel-control-prev' type='button' data-bs-target='#carouselGames' data-bs-slide='prev'>
                     <span className='carousel-control-prev-icon' aria-hidden='true'></span>
@@ -84,28 +49,10 @@ const Game = () => {
                 </button>
             </section>
 
+            <h3 style={{ paddingTop: 100 }} data-aos='zoom-in-down' data-aos-duration='2000'>Remakes</h3>
             <section className='remakes' data-aos='zoom-in-down' data-aos-duration='2000'>
-                <h3>Remakes</h3>
                 <div id='carouselRemakes' className='carousel slide carousel-fade'>
-                    <div className='carousel-inner'>
-                        {remakes.map((post, index) => {
-                            const itemClasses = index === 0 ? 'carousel-item active' : 'carousel-item';
-                            return (
-                                <div className={itemClasses} key={post.id}>
-                                    <div className='card'>
-                                        <img src={post.Image} alt='...' />
-                                        <div className='card-content'>
-                                            <h4 className='card-title'> {post.Title}</h4>
-                                            <span>{post.Time}</span>
-                                            <p className='card-body'>{post.Content}</p>
-                                            <Link to={post.Link} className='btn btn-outline-light' target='_blank'>Read more</Link>
-                                        </div>
-                                    </div>
-                                </div>
-                            )
-                        })}
-
-                    </div>
+                    <div className='carousel-inner'>{renderCarouselItems(remakes)}</div>
                 </div>
                 <button className='carousel-control-prev' type='button' data-bs-target='#carouselRemakes' data-bs-slide='prev'>
                     <span className='carousel-control-prev-icon' aria-hidden='true'></span>
@@ -117,33 +64,18 @@ const Game = () => {
                 </button>
             </section>
 
+            <h3 style={{paddingTop: 100}}  data-aos='zoom-in-right' data-aos-duration='2000'>Spin-offs</h3>
+            <p style={{paddingTop: 20}}  data-aos='zoom-in-right' data-aos-duration='2000'>
+                Here listed some well-known spin-offs of The Legend of Zelda series.
+                <br /> For more information about other spin-offs, click{' '}
+                <Link to='https://zeldawiki.wiki/wiki/Main_Page' target='_blank' style={{ color: 'white' }}>
+                    here
+                </Link>
+                .
+            </p>
             <section className='spinoffs' data-aos='zoom-in-right' data-aos-duration='2000'>
-                <div>
-                    <h3>Spin-offs</h3>
-                    <p>Here listed some well-known spin-offs of The Legend of Zeelda series.
-                        <br /> For more information about other spin-offs, click <Link to='https://zeldawiki.wiki/wiki/Main_Page' target='_blank' style={{ color: 'white' }}> here</Link>.
-                    </p>
-                </div>
                 <div id='carouselSpinoffs' className='carousel slide carousel-fade'>
-                    <div className='carousel-inner'>
-                        {spinoffs.map((post, index) => {
-                            const itemClasses = index === 0 ? 'carousel-item active' : 'carousel-item';
-                            return (
-                                <div className={itemClasses} key={post.id}>
-                                    <div className='card'>
-                                        <img src={post.Image} alt='...' />
-                                        <div className='card-content'>
-                                            <h4 className='card-title'> {post.Title}</h4>
-                                            <span>{post.Time}</span>
-                                            <p className='card-body'>{post.Content}</p>
-                                            <Link to={post.Link} className='btn btn-outline-light' target='_blank'>Read more</Link>
-                                        </div>
-                                    </div>
-                                </div>
-                            )
-                        })}
-
-                    </div>
+                    <div className='carousel-inner'>{renderCarouselItems(spinoffs)}</div>
                 </div>
                 <button className='carousel-control-prev' type='button' data-bs-target='#carouselSpinoffs' data-bs-slide='prev'>
                     <span className='carousel-control-prev-icon' aria-hidden='true'></span>
